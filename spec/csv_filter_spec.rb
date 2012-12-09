348308be6253921d1d031834178c6414126ea0f5
Kris Luminar <kris.luminar@gmail.com>
2012-12-09 13:42:18 -0800

    describe the behaviour

--- spec/csv_filter_spec.rb
+++ spec/csv_filter_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "CsvFilter" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+  it "should create a new object from a file"
+  it "should grab just the columns specified"
+  it "should return just the columns specified"
+  it "should send output to stndout"
+  describe "to_file"
+    it "should create the named file"
+    it "should send success message to stdout"
+    it "should not send the file contents to stdout"
+    describe "file contents"
+      it "should include just the columns specified"
+  describe 'grep'
+    it "should grep for a string in rows"
+    it "should only show the grepped rows"
 end
