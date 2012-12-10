39a012b554a1f2f3146b8646f9817ec85a5f4a88
Kris Luminar <kris.luminar@gmail.com>
2012-12-10 12:29:55 -0800

    - a bunch of exploratory coding that culminated in a working filter method   - See specs for details.

--- lib/csv_filter.rb
+++ lib/csv_filter.rb
@@ -1,8 +1,58 @@
 # @author Kris Luminar
-# @email kris.luminar@gmail.com
 class CsvFilter
   # @param file_path the full path to a file
-  def initialize file_path
+  def initialize file_path, separator = "\t"
     @file = File.open(file_path, 'r')
+    @separator = separator
+    @num_columns = count_columns
+    @header = {}
+    grab_header
   end
+
+  def grab_header
+    return @header if (@header and !@header.empty?)
+    @file.rewind
+    fields.each_with_index do |field_name, i|
+      @header[i]= field_name.strip
+    end
+    @header
+  end
+
+  def filtered_column_positions columns
+    columns = columns.flatten
+    @filtered_column_positions ||= register.select {|field, pos| columns.include? field }.values
+  end
+
+  def register
+    grab_header.invert
+  end
+
+  def filter(*columns)
+    # columns = [*columns] #columns should accept either an array of strings or a variable number of strings
+    raise ArgumentError unless (columns.respond_to?(:size) and columns.size < @num_columns)
+    output = []
+    @file.each_with_index do |line, i|
+      #TODO: Decide whether to allow user to specify if header row exists. If so, this step will be conditional. Else, add proviso to the README that csv file must include a header line.
+      next if i == 0 # skip header row
+      row = {}
+      line.split(@separator).each_with_index do |value, j|
+        if filtered_column_positions(columns).include? j
+          row[@header[j]] = value
+        end
+        output << row
+      end
+    end
+    output
+  end
+
+  def count_columns
+    fields.size
+  end
+
+  def fields
+    return @fields if @fields
+    @file.rewind
+    @fields = @file.gets.split(@separator).map &:strip
+  end
+
 end
--- spec/csv_filter_spec.rb
+++ spec/csv_filter_spec.rb
@@ -5,10 +5,43 @@ describe "CsvFilter" do
     @tsv = CsvFilter.new(File.expand_path(File.dirname(__FILE__) + '/sample.tsv'))
   end
 
-  it "should grab just the columns specified" do
-    columns = @tsv.filter('value', 'url')
-    (columns.gets =~ 'value').should be_true
+  it "#filter should grab just the columns specified" do
+    fields = ['value', 'url']
+    columns = @tsv.filter(fields)
+    columns.first.keys.should eq fields
   end
+
+  it "should know how many columns exist" do
+    @tsv.count_columns.should be 4
+  end
+
+  # it "#filter should accept either an array of strings or a variable number of strings" do
+  #   fields = ['value', 'url']
+  #   columns = @tsv.filter(fields)
+  #   columns.first.keys.should eq fields
+
+  #   columns = @tsv.filter('value', 'url')
+  #   columns.first.keys.should eq fields
+  # end
+
+  it "should retrieve the header row" do
+    header = @tsv.grab_header
+    header.should be_an_instance_of(Hash)
+    header.values.should eq @tsv.fields
+  end
+
+  it "should know the position of columns based on name" do
+    @tsv.filtered_column_positions(['value', 'url']).should eq [0, 2]
+  end
+
+  it "should know the columns and their positions" do
+    register = @tsv.register
+    register.should be_an_instance_of Hash
+    register.keys.should eq @tsv.fields
+    register['url'].should eq 2
+  end
+
+  it "should return a warning if all args not in header line" #exit code and puts warning rather than exception
 
   it "should grep for a string in rows"
   describe "console output"
