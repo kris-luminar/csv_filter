9139f9240bcbf2103b5f4b9955ab92f077f21f51
Kris Luminar <kris.luminar@gmail.com>
2012-12-11 17:57:16 -0800

    public methods should be documented

--- lib/csv_filter.rb
+++ lib/csv_filter.rb
@@ -10,6 +10,8 @@ class CsvFilter
     grab_header
   end
 
+  # grab the first row of the inputed csv file
+  #
   def grab_header
     return @header if (@header and !@header.empty?)
     @file.rewind
@@ -19,15 +21,22 @@ class CsvFilter
     @header
   end
 
+  # the positions of all the columns that the user wants filtered on
+  #
   def filtered_column_positions columns
     columns = columns.flatten
     @filtered_column_positions ||= register.select {|field, pos| columns.include? field }.values
   end
 
+  # map column names to their relative positions (numbers)
+  #
   def register
     grab_header.invert
   end
 
+  # selects just the desired columns from a CSV file
+  #
+  # @param columns list of columns to filter on
   def filter(*columns)
     # columns = [*columns].flatten #columns should accept either an array of strings or a variable number of strings
     raise ArgumentError unless (columns.respond_to?(:size) and columns.size < @num_columns)
@@ -46,6 +55,9 @@ class CsvFilter
     output
   end
 
+  # CLI interface for filter
+  #
+  # (see #filter)
   def print_filter(columns)
     lines = filter(columns)
     output = []
@@ -59,10 +71,15 @@ class CsvFilter
     puts output.join("\n")
   end
 
+  # count num of columns
+  # already deprecated, always a bad sign
+  #
   def count_columns
     fields.size
   end
 
+  # list the fields from the header rown in the input file
+  #
   def fields
     return @fields if @fields
     @file.rewind
